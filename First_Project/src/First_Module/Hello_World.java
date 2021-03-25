2774550e7700ffafe3331535e3a97232787c0965
oshani diss123 <oshanivinodya.98@gmail.com>
2021-03-26 01:19:29 +0530

    This is my first time push project by eclipse.

---
+++ First_Project/src/First_Module/Hello_World.java
@@ -0,0 +1,10 @@
+package First_Module;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first project at SLIIT.");
+	}
+
+}
